b31e67067951983f8910f0b2d40abfd1bd6ffa81
ryunosukearima <a.ryunosuke0125@gmail.com>
2026-03-27 21:06:55 +0900

    ログイン切り替え

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom" 
 
 import { DisplayColorContext } from "./context/DisplayColorContext"
 import { Form } from "./components/Form";
 import { app } from "./tv/app.tv"
 import { DisplayMode } from "./components/DisplayMode";
+
+import { AuthProvider } from "./context/AuthContext";
+
+// CRUD機能
+import { LoginPage } from "./pages/LoginPage"
+import { SignupPage } from "./pages/SignupPage";
 
 export default function App() {
   const [displayColor, setDisplayColor] = useState<boolean>(false)
@@ -13,12 +20,23 @@ export default function App() {
 
   return (
     <DisplayColorContext.Provider value={{ displayColor, setDisplayColor}}>
-      <div className={base()}>
-        <div className={frame()}>
-          <DisplayMode />
-          <Form />
-        </div>
-      </div>
+      <AuthProvider>
+        {/* URLを監視してReactにルーティング機能を渡す */}
+        {/* ルーティング機能を提供 */}
+        <BrowserRouter>
+          <div className={base()}>
+            <div className={frame()}>
+              <DisplayMode />
+              <Routes>
+                {/* 上からの情報を受け取る */}
+                <Route path="/" element={<Form />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+              </Routes>
+            </div>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
     </DisplayColorContext.Provider>
   );
 }
---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState } from "react";
+import type { ReactNode } from "react";
+
+type AuthContextType = {
+    token: string | null;
+    login: (token: string) => void;
+    logout: () => void;
+    isLoggedIn: boolean
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode}) => {
+    const[token, setToken] = useState<string | null>(
+        // リロードしても維持
+        localStorage.getItem("token")
+    );
+
+    const login = (newToken: string) => {
+        localStorage.setItem("token", newToken);
+        setToken(newToken);
+    }
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setToken(null);
+    }
+
+    return (
+        <AuthContext.Provider value={{ token, login, logout, isLoggedIn: !!token }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if(!context) throw new Error("useAuth must be used within AuthProvider")
+        return context
+}
