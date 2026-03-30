409f248d68159a0cc94d151aeaa599210a2afb94
ryunosukearima <a.ryunosuke0125@gmail.com>
2026-03-30 09:58:57 +0900

    履歴取得

---
+++ frontend/src/utils/fetchHistory.ts
@@ -0,0 +1,7 @@
+export const fetchHistory = async(token: string) => {
+    const res = await fetch("http://localhost:5000/api/posts", {
+        headers: { Authorization: `Bearer ${token}`},
+    });
+    if(!res.ok) throw new Error(`HTTP ${res.status}`);
+    return res.json();
+}
