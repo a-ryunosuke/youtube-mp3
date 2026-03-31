175d873d0c585e727ff82ef36aa55032162ef0c8
ryunosukearima <a.ryunosuke0125@gmail.com>
2026-03-31 16:14:35 +0900

    MainPage.tsx-追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom" 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import { DisplayColorContext } from "./context/DisplayColorContext"
-import { Form } from "./components/Form";
 import { app } from "./tv/app.tv"
 import { DisplayMode } from "./components/DisplayMode";
+import { MainPage } from "./pages/Mainpage";
 
 import { AuthProvider } from "./context/AuthContext";
 
@@ -15,11 +15,11 @@ import { SignupPage } from "./pages/SignupPage";
 export default function App() {
   const [displayColor, setDisplayColor] = useState<boolean>(false)
   const { base, frame } = app({
-      color: displayColor ? "light" : "dark"
+    color: displayColor ? "light" : "dark"
   })
 
   return (
-    <DisplayColorContext.Provider value={{ displayColor, setDisplayColor}}>
+    <DisplayColorContext.Provider value={{ displayColor, setDisplayColor }}>
       <AuthProvider>
         {/* URLを監視してReactにルーティング機能を渡す */}
         {/* ルーティング機能を提供 */}
@@ -29,7 +29,7 @@ export default function App() {
               <DisplayMode />
               <Routes>
                 {/* 上からの情報を受け取る */}
-                <Route path="/" element={<Form />} />
+                <Route path="/" element={<MainPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
               </Routes>
